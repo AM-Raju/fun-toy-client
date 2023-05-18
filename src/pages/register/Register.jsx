11c39610b2f30eba65ea4a1da2e6654aaadf638a
AM-Raju <mdmorshed.sj2005@gmail.com>
2023-05-18 22:15:41 +0600

    Registration form functionality done

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,41 +1,61 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Register = () => {
+  const { createUser } = useContext(AuthContext);
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, photo, email, password, "Bangladesh");
+
+    createUser(email, password)
+      .then((result) => {
+        const registeredUser = result.user;
+        alert("Registration ok");
+        // Reset registration form after successful registration
+        form.reset();
+      })
+
+      .catch((error) => console.log(error.message));
+  };
+
   return (
     <div className="flex justify-center items-center h-[800px]">
       <div className="bg-[#e5f2fc] py-10 pr-16  w-1/4 border-r-4 border-[#FE7288] ">
         <h3 className="mb-10 pl-16 border-l-4 border-[#FE7288] pt-4 text-4xl font-semibold font-mali">
           Please Register!
         </h3>
-        <form className="flex flex-col space-y-8 py-4 pl-16">
+        <form onSubmit={handleRegister} className="flex flex-col space-y-8 py-4 pl-16">
           <input
             className="py-3 px-3  bg-transparent border-b-2 outline-none focus:bg-[#FE7288] border-[#FE7288] placeholder:text-[#FE7288] focus:text-white text-xl text-[#FE7288]"
             type="text"
             name="name"
             placeholder="Name"
-            id=""
           />
           <input
             className="py-3 px-3  bg-transparent border-b-2 outline-none focus:bg-[#FE7288] border-[#FE7288] placeholder:text-[#FE7288] focus:text-white text-xl text-[#FE7288]"
             type="text"
             name="photo"
             placeholder="Photo URL"
-            id=""
           />
           <input
             className="py-3 px-3  bg-transparent border-b-2 outline-none focus:bg-[#FE7288] border-[#FE7288] placeholder:text-[#FE7288] focus:text-white text-xl text-[#FE7288]"
             type="email"
             name="email"
             placeholder="Email"
-            id=""
           />
           <input
             className="py-3 px-3  bg-transparent border-b-2 outline-none focus:bg-[#FE7288] border-[#FE7288] placeholder:text-[#FE7288] focus:text-white text-xl text-[#FE7288]"
             type="password"
             name="password"
             placeholder="password"
-            id=""
           />
           <input
             className="w-full text-center text-xl pl-5 py-3 bg-[#FE7288] font-semibold "
